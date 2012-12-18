ffd9291b8c3f104d91836f5cf38c22adc9db8972
Jason Miazga <jason@dp-d830-03.definity.local>
2012-12-18 10:09:05 -0500

    now uses getopts instead of ordered arguements

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-svn2Bitbucket
+svn2bitbucket
 =============
 A simple bash script to checkout an svn repository where the root is trunk, create a new repository in Bitbucket, then push to the new repository in Bitbucket.  This requires svn2git, which can be found here: https://github.com/nirvdrum/svn2git
--- svn2bitbucket.sh
+++ svn2bitbucket.sh
@@ -1,23 +1,46 @@
 #! /bin/bash 
-svnUrl=$1
-bitbucketRepoName=$2
-bitbucketRepoNameNoSpaces=${bitbucketRepoName// /-}
-bitbucketUserName=$3
-bitbucketPassword=$4
+
+function usage_and_exit()
+{
+    echo "$0 -s <svnUrl> -n <bitbucketRepoName> -u <bitbucketUserName> -p <bitbucketPassword>"
+    exit 1;
+}
+
+svnUrl=
+bitbucketRepoName=
+bitbucketRepoNameNoSpaces=
+bitbucketUserName=
+bitbucketPassword=
+
+while getopts s:n:u:p: flag
+do
+    case $flag in
+        s)
+            svnUrl=$OPTARG;;
+        n)
+            bitbucketRepoName=$OPTARG
+	    bitbucketRepoNameNoSpaces=${bitbucketRepoName// /-};;
+        u)
+            bitbucketUserName=$OPTARG;;
+        p)
+            bitbucketPassword=$OPTARG;;
+        ?)
+            usage_and_exit;;
+    esac
+done
+
+if [ -z "$svnUrl" ] || [ -z "$bitbucketRepoName" ] || [ -z "$bitbucketUserName" ] || [ -z "$bitbucketPassword" ];
+then
+    echo "missing a required parameter (svnUrl, bitbucketRepoName, bitbucketUserName and bitbucketPassword are required)"
+    usage_and_exit
+fi
 
 command -v svn2git >/dev/null 2>&1 || { echo >&2 "I require svn2git but it's not installed.  Aborting."; exit 1; }
 
-if [ $# != 4 ]; then
-	echo "usage: svn2bitbucket [svnUrl] [bitbucketRepoName] [bitbucketUserName] [bitbucketPassword]"
-else
-	set -x
-	mkdir "$bitbucketRepoName"
-	cd "$bitbucketRepoName"
-	svn2git $svnUrl --trunk / --nobranches --notags -v
-	curl -k -X POST --user $bitbucketUserName:$bitbucketPassword "https://api.bitbucket.org/1.0/repositories" -d "name=$bitbucketRepoName" -d "is_private='true'"
-	git remote add origin git@bitbucket.org:$bitbucketUserName/$bitbucketRepoNameNoSpaces.git
-	git push -u origin master
-	set +x
-fi
-
-
+set -x
+mkdir "$bitbucketRepoName"
+cd "$bitbucketRepoName"
+svn2git $svnUrl --trunk / --nobranches --notags -v
+curl -k -X POST --user $bitbucketUserName:$bitbucketPassword "https://api.bitbucket.org/1.0/repositories" -d "name=$bitbucketRepoName" -d "is_private='true'"
+git remote add origin git@bitbucket.org:$bitbucketUserName/$bitbucketRepoNameNoSpaces.git
+git push -u origin master
